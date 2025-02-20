65cb1720f3a9d2c526ec7f4c62e5ab209fecd6be
Nakul Attrey <nakulattrey@gmail.com>
2025-02-20 22:59:36 +0530

    first commit

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+Theme
+Dark Mode Vibes (Cyan & Slate):
+
+    Primary: cyan-500
+    Secondary: slate-700
+    Background: slate-900
+    Accent: cyan-300
+    Text: white
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import React from "react";
+
+function App() {
+
+  return (
+    <>
+      <div>This is Linkbook</div>
+    </>
+  );
+}
+
+export default App;
